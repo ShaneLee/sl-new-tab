aff96d981e7dbcbf33fcb74b8625f09b2f22d516
Shane <me@shanel.ee>
2023-07-23 21:10:54 +0100

    Add todos

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,5 +1,3 @@
-loadJSon()
-
 document.addEventListener('keydown', (event) => {
   if (event.key === '~') {
     document.getElementById('main').style.display = 'none'
@@ -30,8 +28,8 @@ function pickRandom(quotes) {
 }
 
 function printQuote(quoteAuthor, quote) {
-  document.getElementById("quote").innerHTML = quote
-  document.getElementById("author").innerHTML = quoteAuthor
+  document.getElementById('quote').innerHTML = quote
+  document.getElementById('author').innerHTML = quoteAuthor
 }
 
 function deathCountdown() {
@@ -42,4 +40,65 @@ function deathCountdown() {
 		'You have '.toUpperCase() + days + ' days remaining.'.toUpperCase()
 	document.getElementById('title').innerHTML = days + ' Days Left'
   document.title = days
+}
+
+const todosEndpoint = 'http://localhost:8080/todos'
+const completeEndpoint = 'http://localhost:8080/todos/complete'
+
+
+
+function todoForm() {
+  const form = document.getElementById('todo-form');
+  form.addEventListener('submit', function(event){
+      event.preventDefault();
+    
+      const todo = document.getElementById('todo-input').value;
+      const category = document.getElementById('category-input').value;
+      const formData = JSON.stringify({ 'todo': todo, 'category': category })
+      fetch(todosEndpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: formData
+      }).finally(() => {
+        reloadTodos()
+      });
+  });
+}
+
+
+function reloadTodos() {
+  // TODO make this better
+  location.reload();
+}
+
+function todos() {
+  fetch(todosEndpoint, {method: 'GET'})
+  .then(response => response.json())
+  .then(todos => {
+      const uL = document.getElementById('todos');        
+      todos?.forEach(todo => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('PATCH', completeEndpoint, false);
+        xhr.setRequestHeader('Content-Type', 'application/json')
+
+        const listItem = document.createElement('li');  
+        listItem.className = 'todo-item';
+        listItem.innerHTML = todo.todo;
+        listItem.id = todo.id
+        listItem.addEventListener('click', () => {
+          xhr.send(JSON.stringify(todo))
+          listItem.style = 'display: none;'
+          return false
+        });
+        uL.appendChild(listItem);
+      })
+  });
+}
+
+window.onload = function() {
+  loadJSon()
+  todos()
+  todoForm()
 }
