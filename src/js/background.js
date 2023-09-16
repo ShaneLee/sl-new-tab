020eda350260c9fd71624d1b529e051199719387
Shane <me@shanel.ee>
2023-09-16 12:32:11 +0100

    Make improvements to web tracking and turn on

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,7 @@
 const host = 'http://localhost:8080'
-const webTrackerEndpoint = `${host}/web/track`
-const webTrackingEnabled = false
+const webTrackerEndpoint = `${host}/tracking/web`
+const stopEndpoint = `${host}/tracking/web/stop`
+const webTrackingEnabled = true
 const tempUserId = 'bd11dcc2-77f6-430f-8e87-5839d31ab0e3'
 
 const headers = {
@@ -8,17 +9,20 @@ const headers = {
   'tempUserId': tempUserId
 }
 
-let previousTabURL = null;
+let previousTab = null;
 
 if (webTrackingEnabled) {
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
-      if (tab.url.startsWith("chrome://")) return
-      if (tab.incognito) return
-
+	chrome.tabs.onActivated.addListener(activeInfo => {
+    chrome.tabs.get(activeInfo.tabId, (tab) => {
+      if (tab.url.startsWith("chrome://") || tab.incognito) {
+        if (previousTab) {
+          endTracking(previousTab);
+        }
+        return
+      }
 
       chrome.scripting.executeScript({
-        target: {tabId: tabId},
+        target: {tabId: activeInfo.tabId},
         function: getTabURL,
       }, (results) => {
         if (chrome.runtime.lastError) {
@@ -28,20 +32,15 @@ if (webTrackingEnabled) {
 
         const currentURL = new URL(results[0].result)?.hostname;
         
-        sendToEndpoint(currentURL);
         
-        if (previousTabURL === currentURL) {
+        if (previousTab?.url === currentURL) {
           // We haven't changed pages
           return
         }
         
-        if (previousTabURL) {
-          sendToEndpoint(previousTabURL);
-        }
-        
-        previousTabURL = currentURL;
+        startTracking(currentURL);
       });
-    }
+    })
   });
 }
 
@@ -58,19 +57,35 @@ function getTabURL() {
   return window.location.href;
 }
 
-function sendToEndpoint(url) {
-	console.log(url)
-	return 
-	
-  fetch(webtrackerEndpoint, {
+function startTracking(url) {
+  fetch(webTrackerEndpoint, {
     method: 'POST',
     headers: headers,
     body: JSON.stringify({ url: url }),
   })
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    if (data && previousTab) {
+      endTracking(data);
+    }
+    if (data) {
+      previousTab = data;
+    }
+    return data
   })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+function endTracking(previous) {
+  fetch(stopEndpoint, {
+    method: 'PATCH',
+    headers: headers,
+    body: JSON.stringify(previous),
+  })
+  .then(response => response.json())
+  .then(data => { })
   .catch((error) => {
     console.error('Error:', error);
   });
