0458c89078d789086dc2b3e71ad2a544d4c9aded
Shane <me@shanel.ee>
2023-09-18 19:07:13 +0100

    Add basic idea bucket

---
+++ src/js/bucket.js
@@ -0,0 +1,63 @@
+host='http://localhost:8080'
+
+const tempUserId = 'bd11dcc2-77f6-430f-8e87-5839d31ab0e3'
+
+const bucketEndpoint = `${host}/bucket`
+
+const headers = {
+  'Content-Type': 'application/json',
+  'tempUserId': tempUserId
+}
+
+function getIdeas() {
+  fetch(bucketEndpoint, {
+      method: 'GET',
+      headers: headers
+      })
+  .then(response => response.status === 200 ? response?.json() : null)
+  .then(val => {
+    if (!!val) {
+      populateIdeaTable(val)
+    }
+  });
+}
+
+
+function populateIdeaTable(ideas) {
+  const tbody = document.getElementById('idea-bucket-table');
+
+  // Clear the current content of the tbody
+  tbody.innerHTML = '';
+
+  ideas.forEach(idea => {
+      const row = document.createElement('tr');
+
+      const ideaCell = document.createElement('td');
+      ideaCell.textContent = idea.idea;
+      row.appendChild(ideaCell);
+
+      const categoryCell = document.createElement('td');
+      categoryCell.textContent = idea.category;
+      row.appendChild(categoryCell);
+
+      const notesCell = document.createElement('td');
+      if (idea.notes && idea.notes.length > 50) {
+          notesCell.textContent = idea.notes.substring(0, 47) + '...';
+      } else {
+          notesCell.textContent = idea.notes || '';
+      }
+      row.appendChild(notesCell);
+
+      row.addEventListener('click', () => {
+          // Assuming a separate HTML page named 'details.html' that will handle displaying the form
+          // We pass the idea ID via URL parameter so that we can fetch and display the details
+          window.location.href = 'details.html?id=' + idea.id;
+      });
+
+      tbody.appendChild(row);
+  });
+
+}
+
+
+window.addEventListener("load", getIdeas);
