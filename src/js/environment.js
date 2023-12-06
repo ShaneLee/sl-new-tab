bde8f21ac612f554f8fe97f5f9ea43961d9584dd
Shane <me@shanel.ee>
2023-12-06 17:25:11 +0000

    Add css variables with basic theme

--- src/js/environment.js
+++ src/js/environment.js
@@ -44,3 +44,25 @@ function api(endpoint, obj, rethrow) {
   return fetch(endpoint, obj)
     .catch(err => null)
 }
+
+const defaultTheme = new Map()
+defaultTheme.set("--background-color", "#272725")
+defaultTheme.set("--text-color", "#8d8271")
+defaultTheme.set("--link-color", "#8d8271")
+defaultTheme.set("--link-hover-color", "#f0f0f0")
+defaultTheme.set("--highlighted-todo-color", "#f0f0f0")
+defaultTheme.set("--font-family", "'Helvetica Neue', sans-serif")
+defaultTheme.set("--main-color", "#e7b91b")
+defaultTheme.set("--darker-main-color", "#d1a116")
+defaultTheme.set("--dark-grey", "#2c2c2c")
+defaultTheme.set("--light-grey", "#868680")
+
+
+const darkerTheme = new Map()
+darkerTheme.set("--background-color", "#131313")
+
+function setTheme(theme) {
+  console.log('call')
+  const root = document.documentElement
+  theme.forEach((value, key) => root.style.setProperty(key, value))
+}
