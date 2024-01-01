ed4e624583899de6ddd8069c10a552b57803fe19
Shane <me@shanel.ee>
2024-01-01 07:51:29 +0000

    Fix bug with week 0

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -353,7 +353,7 @@ function currentWeekNumber() {
   const startDate = new Date(currentDate.getFullYear(), 0, 1);
   const  days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
 
-  const weekNumber = Math.ceil(days / 7);
+  const weekNumber = Math.max(1, Math.ceil(days / 7));
 
   return weekNumber
 }
@@ -383,7 +383,7 @@ function categories() {
     const all = document.createElement('option');  
     defaultCategory.className = 'categories-item';
     all.className = 'categories-item';
-      all.innerHTML = 'all'
+    all.innerHTML = 'all'
     const week = `Week ${currentWeekNumber()}`
     if (defaultToAll) {
       categories.appendChild(all)
