7caefc8ae81f22f67900a11205e0f7d19dce4c1c
Shane <me@shanel.ee>
2024-05-06 09:45:36 +0100

    Add idea bucket context menu and copy to todo

--- src/js/bucket.js
+++ src/js/bucket.js
@@ -1,3 +1,6 @@
+let contextMenu;
+let selectedIdea;
+
 function getIdeas() {
   fetch(bucketEndpoint, {
       method: 'GET',
@@ -11,6 +14,46 @@ function getIdeas() {
   });
 }
 
+function createTodo(todo) {
+  return api(todosEndpoint, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(todo)
+  })
+
+  .then(response => response.status === 200 || response.status === 201 ? response?.json() : null)
+}
+
+function createIdea(idea) {
+  return api(bucketEndpoint, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(idea)
+  })
+
+  .then(response => response.status === 200 || response.status === 201 ? response?.json() : null)
+}
+
+function deleteIdea(idea) {
+  return api(bucketEndpoint, {
+    method: 'DELETE',
+    headers: headers,
+    body: JSON.stringify(idea)
+  })
+
+  .then(response => response.status === 204 ? response?.json() : null)
+}
+
+function update(idea) {
+  return api(bucketEndpoint, {
+    method: 'PUT',
+    headers: headers,
+    body: JSON.stringify(idea)
+  })
+
+  .then(response => response.status === 200 || response.status === 201 ? response?.json() : null)
+}
+
 function addIdeaFormListener() {
   document.getElementById('ideaForm').addEventListener('submit', function (event) {
     event.preventDefault();
@@ -21,15 +64,15 @@ function addIdeaFormListener() {
       jsonObject[key] = value;
     });
 
-    api(bucketEndpoint, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(jsonObject)
-    })
-
-    .then(response => response.status === 200 || response.status === 201 ? response?.json() : null)
+    createIdea(jsonObject)
   });
 }
+  // listItem.addEventListener('contextmenu', function(event) {
+  //   if (!!contextMenu) {
+  //     hideContextMenu()
+  //   }
+  //   showContextMenu(event, idea);
+  // });
 
 
 function populateIdeaTable(ideas) {
@@ -56,6 +99,12 @@ function populateIdeaTable(ideas) {
           notesCell.textContent = idea.notes || '';
       }
       row.appendChild(notesCell);
+      row.addEventListener('contextmenu', function(event) {
+        if (!!contextMenu) {
+          hideContextMenu()
+        }
+        showContextMenu(event, idea);
+      });
 
       row.addEventListener('click', () => {
           // Assuming a separate HTML page named 'details.html' that will handle displaying the form
@@ -68,6 +117,116 @@ function populateIdeaTable(ideas) {
 
 }
 
+function showContextMenu(event, idea) {
+  // Check if the right-click occurred outside of an 'a' tag
+  if (event.target.tagName.toLowerCase() === 'a') {
+    return;
+  }
+  event.preventDefault();
+  selectedIdea = idea;
+  const contextMenuId = 'ideaContextMenu';
+  contextMenu = document.getElementById(contextMenuId);
+
+  // Ensure the context menu is visible before retrieving dimensions
+  contextMenu.style.display = 'block';
+  
+  const contextMenuWidth = contextMenu.offsetWidth;
+  const contextMenuHeight = contextMenu.offsetHeight;
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+  
+  let left = event.clientX;
+  let top = event.clientY;
+  
+  // Adjust left position if the context menu goes off the right edge
+  if (left + contextMenuWidth > viewportWidth) {
+    left = viewportWidth - contextMenuWidth;
+  }
+  
+  // Adjust top position if the context menu goes off the bottom edge
+  if (top + contextMenuHeight > viewportHeight) {
+    top = viewportHeight - contextMenuHeight;
+  }
+  
+  // Ensure the top position is never negative
+  top = Math.max(top, 0);
+  
+  contextMenu.style.left = `${left}px`;
+  contextMenu.style.top = `${top}px`;
+  
+  event.stopPropagation();
+}
+
+function hideContextMenu() {
+  if (!!contextMenu) {
+    contextMenu.style.display = 'none';
+    contextMenu = null
+  }
+}
+
+function addIdeaListener() {
+
+  contextMenu = document.getElementById('ideaContextMenu');
+  const deleteAction = document.getElementById('deleteAction');
+  const editAction = document.getElementById('editAction');
+  const changeCategoryAction = document.getElementById('changeCategoryAction');
+  const copyToTodoAction = document.getElementById('copyToTodoAction');
+
+  // No global context menu for ideas
+  // document.addEventListener('contextmenu', function(event) {
+  //   hideContextMenu()
+  //   showContextMenu(event)
+  // });
+
+  deleteAction.addEventListener('click', function() {
+    deleteIdea(selectedIdea, true)
+    selectedIdea = null
+    hideContextMenu();
+  });
+
+  copyToTodoAction.addEventListener('click', function() {
+    const idea = selectedIdea
+    const todo = { 'todo': idea.idea }
+    const category = prompt('Enter the new category:');
+    if (!!category) {
+      todo.category = category
+    }
+    createTodo(todo)
+    selectedIdea = null
+    hideContextMenu();
+  });
+
+  changeCategoryAction.addEventListener('click', function() {
+    const idea = selectedIdea
+    const category = prompt('Enter the new category:');
+    if (!!category) {
+      idea.category = category
+      update(idea)
+    }
+    selectedIdea = null
+    hideContextMenu();
+  });
+
+  editAction.addEventListener('click', function() {
+    const idea = selectedIdea
+    const task = prompt('Edit idea:', idea.idea);
+    if (!!task && idea.idea !== task) {
+      idea.idea = task
+      update(idea)
+    }
+    selectedIdea = null
+    hideContextMenu();
+  });
+
+  // Event listener to hide context menu on window click
+  window.addEventListener('click', function() {
+    hideContextMenu();
+  });
+}
+
 
 window.addEventListener("load", getIdeas);
 window.addEventListener("load", addIdeaFormListener);
+window.onload = function() {
+  addIdeaListener()
+}
