93136cc5a4473d17f42aa3974a2f90a9afee4ce2
Shane <me@shanel.ee>
2024-08-11 18:36:28 +0100

    Fix other pages for Firefox

--- src/package-firefox.sh
+++ src/package-firefox.sh
@@ -4,6 +4,13 @@ set -e
 
 mv manifest.json manifest.v3.json
 mv manifest.v2.json manifest.json
+cp manifest.json manifest.v2.json.bak
+files=$(ls template | awk '{printf "\"template/%s\",", $0}' | sed 's/,$//')
+echo "[$files]" > files_array.json
+jq --argjson files "$(cat files_array.json)" '.web_accessible_resources = $files' manifest.json > manifest.updated.json
+mv manifest.updated.json manifest.json
+cat files_array.json
+rm files_array.json
 zip -r -FS ../sl.zip * --exclude '*.git*'
-mv manifest.json manifest.v2.json
+mv manifest.v2.json.bak manifest.v2.json
 mv manifest.v3.json manifest.json
