a4aaf7267878862087c04647f3b886d7fcb65acf
Shane <me@shanel.ee>
2024-08-19 19:16:41 +0100

    Make more progress with adding meals

--- src/js/meals.js
+++ src/js/meals.js
@@ -1,4 +1,4 @@
-let foodItems = []
+let foodItems = [];
 
 function createMealPlan(mealPlan) {
   return api(mealPlanEndpoint, {
@@ -114,6 +114,16 @@ function createFoodItem(foodItem) {
   });
 }
 
+function populateFoodItems(foodItems) {
+  const select = document.getElementById("foodSelect");
+  foodItems.forEach((item) => {
+    const option = document.createElement("option");
+    option.value = item.id;
+    option.textContent = item.name;
+    select.appendChild(option);
+  });
+}
+
 function addFoodItemFormListener() {
   document
     .getElementById("foodItemForm")
@@ -152,137 +162,167 @@ function addFoodItemFormListener() {
       createFoodItem(foodItem);
     });
 }
-function createMeal(meal) {
-  return api(mealEndpoint, {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(meal),
-  }).then((response) => {
-    if (response) {
-      return response.json();
-    }
-  });
+
+function addFoodItemOptions() {
+  api(foodItemEndpoint, { method: "GET", headers: headers })
+    .then((res) => (!!res ? res.json() : undefined))
+    .then((fetchedFoodItems) => {
+      foodItems = fetchedFoodItems;
+      populateFoodItems(foodItems);
+
+      document
+        .getElementById("addFoodItem")
+        .addEventListener("click", addFoodItem);
+    });
+
+  addMealFormListener();
 }
 
-function populateFoodItems(foodItems) {
-  const select = document.getElementById("foodSelect");
+function updateNutrientInfoAndTotals() {
+  const totals = {
+    calories: 0,
+    carbohydrate: 0,
+    fat: 0,
+    protein: 0,
+  };
+
+  document.querySelectorAll(".food-item").forEach((itemDiv, index) => {
+    const selectElement = document.getElementById(`foodItemSelect-${index}`);
+    const sliderElement = document.getElementById(`foodItemSlider-${index}`);
+    const sliderValue = parseFloat(sliderElement.value);
+    const selectedFoodId = selectElement.value;
+    const foodItem = foodItems.find((item) => item.id === selectedFoodId);
+
+    if (foodItem) {
+      const updatedNutrients = {
+        calories: nutrientValueCalculation(
+          foodItem.nutrients.nutrients.CALORIES,
+          sliderValue,
+          foodItem.nutrients.nutrients.CALORIES
+        ),
+        carbohydrate: nutrientValueCalculation(
+          foodItem.nutrients.nutrients.CARBOHYDRATE,
+          sliderValue,
+          foodItem.nutrients.nutrients.CARBOHYDRATE
+        ),
+        fat: nutrientValueCalculation(
+          foodItem.nutrients.nutrients.FAT,
+          sliderValue,
+          foodItem.nutrients.nutrients.FAT
+        ),
+        protein: nutrientValueCalculation(
+          foodItem.nutrients.nutrients.PROTEIN,
+          sliderValue,
+          foodItem.nutrients.nutrients.PROTEIN
+        ),
+      };
+
+      totals.calories += parseFloat(updatedNutrients.calories.amount);
+      totals.carbohydrate += parseFloat(updatedNutrients.carbohydrate.amount);
+      totals.fat += parseFloat(updatedNutrients.fat.amount);
+      totals.protein += parseFloat(updatedNutrients.protein.amount);
+
+      document.getElementById(
+        `nutrient-info-${index}`
+      ).textContent = `Calories: ${updatedNutrients.calories.amount} ${updatedNutrients.calories.value.unit}
+                Carbohydrate: ${updatedNutrients.carbohydrate.amount} ${updatedNutrients.carbohydrate.value.unit}
+                Fat: ${updatedNutrients.fat.amount} ${updatedNutrients.fat.value.unit}
+                Protein: ${updatedNutrients.protein.amount} ${updatedNutrients.protein.value.unit}`;
+    }
+  });
+
+  document.getElementById("totals").innerHTML = `
+        Total Calories: ${totals.calories.toFixed(2)} UNIT
+        Total Carbohydrate: ${totals.carbohydrate.toFixed(2)} g
+        Total Fat: ${totals.fat.toFixed(2)} g
+        Total Protein: ${totals.protein.toFixed(2)} g
+    `;
+}
+
+function nutrientValueCalculation(nutrient, referenceValue, originalReference) {
+    function convertToBaseUnit(amount, unit) {
+        switch (unit) {
+            case 'µg': return amount / 1e6; // µg to g
+            case 'mg': return amount / 1e3; // mg to g
+            default: return amount
+        }
+    }
+
+    const nutrientAmount = parseFloat(nutrient.value.amount);
+    const nutrientUnit = nutrient.unit;
+    const referenceAmount = parseFloat(referenceValue);
+    const originalReferenceAmount = parseFloat(originalReference.value.amount);
+    const originalReferenceUnit = originalReference.value.unit;
+
+    const baseNutrientAmount = convertToBaseUnit(nutrientAmount, nutrientUnit);
+    const baseOriginalReferenceAmount = convertToBaseUnit(originalReferenceAmount, originalReferenceUnit);
+    const baseReferenceAmount = convertToBaseUnit(referenceAmount, originalReferenceUnit);
+
+    const adjustedAmount = (baseNutrientAmount * baseReferenceAmount / baseOriginalReferenceAmount).toFixed(2);
+
+    return {
+        ...nutrient,
+        amount: adjustedAmount
+    };
+}
+
+function handleSliderChange(event) {
+  updateNutrientInfoAndTotals();
+}
+
+function addFoodItem() {
+  const container = document.getElementById("foodItemsContainer");
+  const index = container.children.length;
+
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("food-item");
+  itemDiv.dataset.index = index;
+
+  const selectElement = document.createElement("select");
+  selectElement.id = `foodItemSelect-${index}`;
+  selectElement.name = `foodItem-${index}`;
+
   foodItems.forEach((item) => {
     const option = document.createElement("option");
     option.value = item.id;
     option.textContent = item.name;
-    select.appendChild(option);
+    selectElement.appendChild(option);
   });
-}
 
-function updateFoodItemDetails(foodItem) {
-  const container = document.getElementById("foodItemDetails");
-  container.innerHTML = "";
+  const sliderElement = document.createElement("input");
+  sliderElement.type = "range";
+  sliderElement.id = `foodItemSlider-${index}`;
+  sliderElement.min = "0";
+  sliderElement.max = "500";
+  sliderElement.value = "100";
 
-  if (foodItem) {
-    const itemDiv = document.createElement("div");
-    itemDiv.classList.add("food-item");
+  const sliderValueElement = document.createElement("span");
+  sliderValueElement.id = `foodItemValue-${index}`;
+  sliderValueElement.textContent = "100";
+  sliderValueElement.style.display = "none";
 
-    const nameLabel = document.createElement("label");
-    nameLabel.textContent = foodItem.name;
-    itemDiv.appendChild(nameLabel);
+  const nutrientInfoElement = document.createElement("div");
+  nutrientInfoElement.id = `nutrient-info-${index}`;
 
-    const amountInput = document.createElement("input");
-    amountInput.type = "number";
-    amountInput.value = 100; // Default amount
-    amountInput.min = 0;
-    amountInput.step = 1;
-    amountInput.id = `amount-${foodItem.id}`;
-    amountInput.dataset.id = foodItem.id;
-    amountInput.style.marginRight = "10px";
-    itemDiv.appendChild(amountInput);
+  selectElement.addEventListener("change", function () {
+    sliderElement.style.display = "block";
+    sliderValueElement.style.display = "inline"; // Show slider when item is selected
+    handleSliderChange();
+  });
 
-    const amountSlider = document.createElement("input");
-    amountSlider.type = "range";
-    amountSlider.value = 100; // Default amount
-    amountSlider.min = 0;
-    amountSlider.max = 1000; // Max value
-    amountSlider.step = 1;
-    amountSlider.id = `slider-${foodItem.id}`;
-    amountSlider.dataset.id = foodItem.id;
-    itemDiv.appendChild(amountSlider);
+  sliderElement.addEventListener("input", function () {
+    sliderValueElement.textContent = sliderElement.value;
+    handleSliderChange();
+  });
 
-    const nutrientInfo = document.createElement("div");
-    nutrientInfo.id = `nutrient-info-${foodItem.id}`;
-    itemDiv.appendChild(nutrientInfo);
+  itemDiv.appendChild(selectElement);
+  itemDiv.appendChild(sliderElement);
+  itemDiv.appendChild(sliderValueElement);
+  itemDiv.appendChild(nutrientInfoElement);
 
-    container.appendChild(itemDiv);
+  container.appendChild(itemDiv);
 
-    // Event listeners
-    amountInput.addEventListener("input", updateNutrientInfo);
-    amountSlider.addEventListener("input", updateNutrientInfo);
-
-    function updateNutrientInfo() {
-      const amount = amountInput.value;
-      const sliderValue = amountSlider.value;
-
-      // Update the input field with the slider value
-      if (amount !== sliderValue) {
-        amountInput.value = sliderValue;
-      }
-
-      const originalReference = foodItem.reference;
-      const nutrients = foodItem.nutrients.nutrients;
-      const updatedNutrients = {
-        calories: nutrientValueCalculation(nutrients.CALORIES, sliderValue, originalReference),
-        carbohydrate: nutrientValueCalculation(nutrients.CARBOHYDRATE, sliderValue, originalReference),
-        fat: nutrientValueCalculation(nutrients.FAT, sliderValue, originalReference),
-        protein: nutrientValueCalculation(nutrients.PROTEIN, sliderValue, originalReference)
-    };
-
-    const nutrientText = `
-        Calories: ${updatedNutrients.calories.amount} ${updatedNutrients.calories.value.unit}
-        Carbohydrate: ${updatedNutrients.carbohydrate.amount} ${updatedNutrients.carbohydrate.value.unit}
-        Fat: ${updatedNutrients.fat.amount} ${updatedNutrients.fat.value.unit}
-        Protein: ${updatedNutrients.protein.amount} ${updatedNutrients.protein.value.unit}
-    `;
-      document.getElementById(`nutrient-info-${foodItem.id}`).textContent =
-        nutrientText;
-    }
-  }
-}
-
-function nutrientValueCalculation(nutrient, referenceValue, originalReference) {
-  function convertToBaseUnit(amount, unit) {
-    switch (unit) {
-      case "ΜG":
-        return amount / 1e6; // µg to g
-      case "MG":
-        return amount / 1e3; // mg to g
-      default:
-        return amount;
-    }
-  }
-
-  const nutrientAmount = parseFloat(nutrient.value.amount);
-  const nutrientUnit = nutrient.value.unit;
-  const referenceAmount = parseFloat(referenceValue);
-  const originalReferenceAmount = parseFloat(originalReference.amount);
-  const originalReferenceUnit = originalReference.unit;
-
-  const baseNutrientAmount = convertToBaseUnit(nutrientAmount, nutrientUnit);
-  const baseOriginalReferenceAmount = convertToBaseUnit(
-    originalReferenceAmount,
-    originalReferenceUnit
-  );
-  const baseReferenceAmount = convertToBaseUnit(
-    referenceAmount,
-    originalReferenceUnit
-  );
-
-  const adjustedAmount = (
-    (baseNutrientAmount * baseReferenceAmount) /
-    baseOriginalReferenceAmount
-  ).toFixed(2);
-
-  return {
-    ...nutrient,
-    amount: adjustedAmount, 
-  };
+  updateNutrientInfoAndTotals();
 }
 
 function addMealFormListener() {
@@ -294,13 +334,13 @@ function addMealFormListener() {
       const formData = new FormData(this);
       const components = [];
 
-      // Collect components
-      document.querySelectorAll(".food-item").forEach((itemDiv) => {
-        const amountInput = itemDiv.querySelector('input[type="number"]');
-        if (amountInput && amountInput.value > 0) {
+      formData.getAll("components[]").forEach((amount, index) => {
+        const foodItemId = document.querySelector(`#amount-${index}`).dataset
+          .id;
+        if (amount) {
           components.push({
-            id: amountInput.dataset.id,
-            amount: `${amountInput.value}:g`,
+            id: foodItemId,
+            amount: amount,
           });
         }
       });
@@ -312,28 +352,6 @@ function addMealFormListener() {
 
       createMeal(meal);
     });
-}
-
-function addFoodItemOptions() {
-  api(foodItemEndpoint, { method: "GET", headers: headers })
-    .then((res) => (!!res ? res.json() : undefined))
-    .then((foodItems) => {
-      populateFoodItems(foodItems);
-
-      document
-        .getElementById("foodSelect")
-        .addEventListener("change", function () {
-          const selectedId = this.value;
-          if (selectedId) {
-            const selectedItem = foodItems.find(
-              (item) => item.id === selectedId
-            );
-            updateFoodItemDetails(selectedItem);
-          }
-        });
-    });
-
-  addMealFormListener();
 }
 
 window.addEventListener("load", addFoodItemOptions);
